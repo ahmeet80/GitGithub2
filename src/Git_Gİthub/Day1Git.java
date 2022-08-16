5951603ae844b680fb95c70ef614014aa9a0dbe2
Ahmet KARAASLAN <ahmeet80@gmail.com>
2022-08-16 20:31:36 +0300

    V120:31

---
+++ src/Git_Gİthub/Day1Git.java
@@ -0,0 +1,8 @@
+package Git_Gİthub;
+
+public class Day1Git {
+    public static void main(String[] args) {
+        System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 1. Denemeeeee"  );
+    }
+}
